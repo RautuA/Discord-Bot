fccead57e1cb78db3aa1efff3f830865a34250a0
rautua <rautualexandruflorin@gmail.com>
2024-06-18 22:03:29 +0300

    Basic Discord bot setup with on_ready event

--- Bot.py
+++ Bot.py
@@ -1,1 +1,12 @@
-Test
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = 'hihi!')
+
+@client.event
+async def on_ready():
+    print("The bot is now ready for use!")
+    print("---------------------")
+    
+@client.command()
+    
